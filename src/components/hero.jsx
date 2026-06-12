053c16e56102a8fc3b89b7f7f481a07e8633246a
Suvalakshmi <suvasneha06@gmail.com>
2026-06-12 22:03:38 +0530

    responsive changes new

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,7 @@ import Logo from "../assets/Duree_Logo.jpg";
 
 export default function Hero() {
   return (
-    <section className="relative min-h-screen overflow-hidden ">
+    <section className="relative h-[120vh] overflow-hidden ">
 
       {/* VIDEO BACKGROUND */}
       <div className="absolute inset-0">
@@ -20,13 +20,13 @@ export default function Hero() {
       {/* HEADER */}
       <header className="absolute top-0 left-0 z-50 w-full">
 
-        <div className="flex items-center justify-between px-4 md:px-8 lg:px-16 py-3">
+        <div className="flex items-center justify-between px-8 lg:px-16 py-2">
 
           {/* LOGO */}
           <img
             src={Logo}
             alt="Logo"
-            className="h-12 md:h-16 object-contain"
+            className="h-16 object-contain"
           />
 
           {/* RIGHT */}
@@ -66,62 +66,62 @@ export default function Hero() {
           </div>
 
         </div>
-        <div className="h-px w-full bg-white/30"></div>
+  <div className="h-px w-full bg-white/30"></div>
 
       </header>
 
 
       {/* HERO CONTENT */}
-      <div className="relative z-20 flex h-full items-center">
+     <div className="relative z-20 flex h-full items-center">
 
-        <div className="mx-auto w-full max-w-7xl px-8 lg:px-16">
+  <div className="mx-auto w-full max-w-7xl px-8 lg:px-16">
 
-          <div className="max-w-4xl mx-auto px-4 lg:px-0 lg:ml-[20%]">
+    <div className="max-w-4xl mx-auto ml-[15%] ">
 
-            <h1
-              className="
+      <h1
+        className="
           text-white
           font-extralight
           leading-[0.85]
           tracking-[-4px]
         "
-            >
-              <span className="block text-5xl sm:text-6xl md:text-7xl lg:text-[6rem]">
-                The
-              </span>
+      >
+        <span className="block text-7xl md:text-8xl lg:text-[6rem]">
+          The
+        </span>
 
-              <span className="block text-5xl sm:text-6xl md:text-7xl lg:text-[6rem]">
-                Heritage Crafted
-              </span>
+        <span className="block text-7xl md:text-8xl lg:text-[6rem]">
+          Heritage Crafted
+        </span>
 
-              <span className="block text-5xl sm:text-6xl md:text-7xl lg:text-[6rem]">
-                In Stone
-              </span>
-            </h1>
+        <span className="block text-7xl md:text-8xl lg:text-[6rem]">
+           In Stone
+        </span>
+      </h1>
 
-            <p
-              className="
+      <p
+        className="
           mt-8
           text-xl
           md:text-2xl
           text-white/90
         "
-            >
-              Authentic Athangudi tiles crafted for timeless interiors
-            </p>
+      >
+        Authentic Athangudi tiles crafted for timeless interiors
+      </p>
 
-          </div>
+    </div>
 
-        </div>
+  </div>
 
-      </div>
-      <div className="absolute bottom-4 left-1/2 z-30 -translate-x-1/2">
+</div>
+<div className="absolute bottom-4 left-1/2 z-30 -translate-x-1/2">
 
-        <button className="bubble-btn">
-          EXPLORE
-        </button>
+  <button className="bubble-btn">
+    EXPLORE
+  </button>
 
-      </div>
+</div>
     </section>
   );
 }
